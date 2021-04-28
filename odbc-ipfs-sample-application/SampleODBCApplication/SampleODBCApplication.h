efe8eda5c5263ffa12b17c12c722565ebbaa68ee
Thomas <thomasctromp@gmail.com>
2021-04-28 09:33:35 +0900

    Topic/update example application (#60)
    * Removed sqlite references
    Refactor
    
    * Delete odbc-ipfs-sample-application/SampleODBCApplication/sqlite64 directory
    
    * Delete Test.cpp
    
    * Delete test.db
    
    * Delete SampleTest.cpp
    
    * Delete test2.db
    
    * Delete SampleODBCApplicationTest.cpp

---
+++ odbc-ipfs-sample-application/SampleODBCApplication/SampleODBCApplication.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<windows.h>
+#include <iostream>
+#include<stdio.h>
+
+#include <sql.h>
+#include <sqlext.h>
+
+void listSQLDrivers();
+void listDataSources();
+SQLHDBC conn(SQLHENV* env);
+void getData(SQLHDBC dbc);

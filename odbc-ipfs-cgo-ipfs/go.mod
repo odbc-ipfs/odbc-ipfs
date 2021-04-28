0caa79662351694eafbb7f55f061e0b4273be99e
Thomas <thomasctromp@gmail.com>
2021-04-28 09:36:37 +0900

    Pull develop into main to update documentation (#59)
    * Create doc.go
    
    * Create doc.go
    
    * Created go.mod and updated source
    
    * Created go.mod and updated source
    
    * bump CI/CD
    
    * Update msbuild.yml
    
    * Added working docker container for go node
    
    * Added example application
    
    * Update docker-compose.yml
    
    * Update docker-compose.yml
    
    Update ports and set correct IPFS version
    
    * Working unit tests using the full system. Note: a go node must be running. (e.g running the docker-compose script)
    
    * Change Installer Odbc Name to odbc-ipfs
    
    * Topic/sql bind col (#56)
    
    * Basic error checking for now
    
    * Added BINDCOL data structure and SQLBindCol function
    
    * SQLBindCOL and SQLFetch written
    
    * Quick Fix to SQLFETCH
    
    Co-authored-by: UcheOZO <uozor@DESKTOP-FN3GJ0O.localdomain>
    
    * Initialized go mod and go sum for cgo connector (#57)
    
    * Initialized go mod and go sum for cgo connector
    
    * Bump CI
    
    * Update msbuild.yml (#58)
    
    * Topic/update example application (#60)
    
    * Removed sqlite references
    Refactor
    
    * Delete odbc-ipfs-sample-application/SampleODBCApplication/sqlite64 directory
    
    * Delete Test.cpp
    
    * Delete test.db
    
    * Delete SampleTest.cpp
    
    * Delete test2.db
    
    * Delete SampleODBCApplicationTest.cpp
    
    Co-authored-by: insoo90 <42658510+insoo90@users.noreply.github.com>
    Co-authored-by: Insoo Hyun <ihyun@purdue.edu>
    Co-authored-by: UcheOZO <42590650+UcheOZO@users.noreply.github.com>
    Co-authored-by: UcheOZO <uozor@DESKTOP-FN3GJ0O.localdomain>

---
+++ odbc-ipfs-cgo-ipfs/go.mod
@@ -0,0 +1,5 @@
+module github.com/odbc-ipfs/odbc-ipfs-cgo-ipfs
+
+go 1.16
+
+require github.com/ipfs/go-ipfs-api v0.2.0
--- odbc-ipfs-unit-test/odbc-ipfs-unit-test.cpp
+++ odbc-ipfs-unit-test/odbc-ipfs-unit-test.cpp
@@ -2,10 +2,12 @@
 #include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-#include <Windows.h>
-#include "..\odbc-ipfs\core.h"
+#include <windows.h>
 #include <sql.h>
 #include <sqlext.h>
+
+//#include "..\odbc-ipfs\core.h"
+#include <Odbcinst.h>
 
 
 namespace odbcipfsunittest
@@ -13,37 +15,82 @@ namespace odbcipfsunittest
 	TEST_CLASS(odbcipfsunittest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			//Assert::AreEqual(1, 1);
-			
-			SQLHDBC dbc;
-			SQLHENV env;
-			SQLRETURN ret;
 
+		SQLHDBC dbc;
+		SQLHENV env;
+		SQLRETURN ret;
+
+		TEST_METHOD_INITIALIZE(TestMethodInit)
+		{			
 			ret = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env);
+			Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLAllocHandle SQL_HANDLE_ENV  Failed");
 
-			Assert::AreEqual(ret, (SQLRETURN) SQL_SUCCESS);
-
-			Assert::IsNotNull(env);
-
-			//SQLSetEnvAttr(env, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0);
+			ret = SQLSetEnvAttr(env, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0);
+			Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLSetEnvAttr SQL_ATTR_ODBC_VERSION  Failed");
 
 			ret = SQLAllocHandle(SQL_HANDLE_DBC, env, &dbc);
 
-			char res[50];
-			sprintf_s(res, "Ret: %d", ret);
-
-			Logger::WriteMessage(res);
-
-			//Assert::AreEqual(ret, (SQLRETURN) SQL_SUCCESS);
+			Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLAllocHandle SQL_HANDLE_DBC Failed");
 
 
-			//Logger::WriteMessage("Testing");
+			SQLWCHAR str_ret[1024];
+			SQLSMALLINT str_ret_len;
+
+			wchar_t str_dsn[] = L"FILEDSN=C:/Program Files/Common Files/Odbc/Data Sources/odbc-ipfs.dsn;";
+
+			ret = SQLDriverConnect(dbc, NULL, str_dsn, SQL_NTS, str_ret, sizeof(4096), &str_ret_len, SQL_DRIVER_COMPLETE);
+
+			Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLDriverConnect Failed");
+		}
+
+		TEST_METHOD_CLEANUP(TestMethodCleanup)
+		{
+			// test method cleanup  code
+			//ret = SQLDisconnect(dbc);
+			//Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLDisconnect Failed");
+
+			//ret = SQLFreeHandle(SQL_HANDLE_DBC, dbc);
+			//Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLFreeHandle(SQL_HANDLE_DBC) Failed");
 			
+			//ret = SQLFreeHandle(SQL_HANDLE_ENV, env);
+			//Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLFreeHandle(SQL_HANDLE_ENV) Failed");
+		}
+		
+		TEST_METHOD(TestSQLAllocHandle)
+		{
+			SQLHSTMT stmt;
 
+			ret = SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
+			//Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLAllocHandle(SQL_HANDLE_STMT) Failed");
+
+			//ret = SQLFreeHandle(SQL_HANDLE_ENV, env);
+			//Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLFreeHandle(SQL_HANDLE_ENV) Failed");
 
 		}
+
+		
+		TEST_METHOD(TestMethod1) //TestSQLExecDirect
+		{	
+
+			
+
+			SQLHSTMT stmt;
+
+			SQLAllocHandle(SQL_HANDLE_STMT, dbc, &stmt);
+
+			ret = SQLExecDirect(stmt, (SQLWCHAR*)L"CREATE TABLE IF NOT EXISTS `new_table1` (`id` INT NOT NULL,`name` VARCHAR(45) NULL);", SQL_NTS); // , PRIMARY KEY(`id`)
+			Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLExecDirect Failed");
+
+			ret = SQLExecDirect(stmt, (SQLWCHAR*)L"INSERT INTO `new_table1` (`id`, `name`) VALUES ('555', 'Hello ECE49595');", SQL_NTS);
+			Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLExecDirect Failed");
+			ret = SQLExecDirect(stmt, (SQLWCHAR*)L"INSERT INTO `new_table1` (`id`, `name`) VALUES ('40', 'dsfdssdfaf');", SQL_NTS);
+			Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLExecDirect Failed");
+
+			ret = SQLExecDirect(stmt, (SQLWCHAR*)L"SELECT * FROM `new_table1`;", SQL_NTS);
+			Assert::AreEqual((SQLRETURN)SQL_SUCCESS, ret, L"SQLExecDirect Failed");
+		}
+
+		
+
 	};
 }
